3d541ed3a9bb0c9eb9ce0c70817badd1ee5bfbd0
Alexandr Yashin <sashayashin37@yandex.ru>
2025-10-30 00:33:31 +0300

    ex2

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 class Employee:
     vacation_days = 28
-    # Вместо инструкции pass напишите свой код.
+
     def __init__ (self, first_name, second_name, gender):
         self.first_name = first_name
         self.second_name = second_name
         self.gender = gender
+        self.remaining_vacation_days = Employee.vacation_days
+
+    def consume_vacation(self, days_to_consume):
+        self.remaining_vacation_days -= days_to_consume
+    
+    def get_vacation_details(self):
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+
         
 
-# Создайте экземпляры класса Employee с различными значениями атрибутов.
 employee1 = Employee(first_name='Александр',second_name='Яшин',gender='м')
-employee2 = Employee(first_name='Елена',second_name='Яровая',gender='ж')
+employee1.consume_vacation(7)
+print(employee1.get_vacation_details())
 
-# Допишите код для вывода информации о сотрудниках.
-print(f'Имя: {employee1.first_name}, Фамилия: {employee1.second_name}, Пол: {employee1.gender}, Отпускных дней в году: {employee1.vacation_days}.')
-print(f'Имя: {employee2.first_name}, Фамилия: {employee2.second_name}, Пол: {employee2.gender}, Отпускных дней в году: {employee2.vacation_days}.')
+
+
+#print(f'Имя: {employee1.first_name}, Фамилия: {employee1.second_name}, Пол: {employee1.gender}, Отпускных дней в году: {employee1.vacation_days}.')
