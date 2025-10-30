4a7b1640a8d902b75473486c2d0f0f1e0a9bddf4
Alexandr Yashin <sashayashin37@yandex.ru>
2025-10-30 09:48:34 +0300

    updates

--- 3.2/Employee.py
+++ 3.2/Employee.py
@@ -6,6 +6,11 @@ class Employee:
         self.second_name = second_name
         self.gender = gender
         self.remaining_vacation_days = Employee.vacation_days
+        self._employee_id = self.__generate_employee_id()
+
+    def __generate_employee_id(self):
+        data_string = self.first_name + self.second_name + self.gender
+        return hash(data_string)
 
     def consume_vacation(self, days):
         self.remaining_vacation_days -= days
@@ -13,20 +18,41 @@ class Employee:
     def get_vacation_details(self):
         return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
 
-# Расширьте класс Employee, создав классы FullTimeEmployee и PartTimeEmployee.
+
 class FullTimeEmployee(Employee):
+    def __init__(self, first_name, second_name, gender, salary):
+        super().__init__(first_name, second_name, gender)
+        self.__salary = salary
+
+    def __get_vacation_salary(self):
+        return self.__salary * 0.8
+
     def get_unpaid_vacation(self, start_date, days):
         return f'Начало неоплачиваемого отпуска: {start_date}, продолжительность: {days} дней.'
+    
+    def get_vacation_payment(self):
+        return self.__get_vacation_salary()
+
 
 class PartTimeEmployee(Employee):
     vacation_days = 14
-    
+
     def __init__(self, first_name, second_name, gender):
         super().__init__(first_name, second_name, gender)
         self.remaining_vacation_days = PartTimeEmployee.vacation_days
 
+
 # Пример использования:
-full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
+full_time_employee = FullTimeEmployee('Иван', 'Иванов', 'м', 50000)
 print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
-part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
+print(f"ID сотрудника: {full_time_employee._employee_id}")
+print(f"Отпускные: {full_time_employee.get_vacation_payment()}")
+
+part_time_employee = PartTimeEmployee('Анна', 'Петрова', 'ж')
+part_time_employee.consume_vacation(5)
 print(part_time_employee.get_vacation_details())
+print(f"ID сотрудника: {part_time_employee._employee_id}")
+
+# Проверка доступа к приватным атрибутам (должно вызывать ошибку)
+# print(full_time_employee.__salary)  # Ошибка!
+# print(full_time_employee.__get_vacation_salary())  # Ошибка!
